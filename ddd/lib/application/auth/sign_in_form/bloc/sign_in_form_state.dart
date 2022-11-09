cc036adaa0e2f4c5cdd2c7d1a5195cf97fe83cd3
YannMLD <malanda70@gmail.com>
2022-11-09 22:34:36 +0100

    Partie 4

---
+++ ddd/lib/application/auth/sign_in_form/bloc/sign_in_form_state.dart
@@ -0,0 +1,21 @@
+part of 'sign_in_form_bloc.dart';
+
+@freezed
+class SignInFormState with _$SignInFormState {
+  const factory SignInFormState({
+    required EmailAddress emailAddress,
+    required Password password,
+    required isSubmiting,
+    required showErrorMessages,
+    required Option<Either<AuthFailure, Unit>> authFailureOrSuccessOption //Option<None, Some> (aka None = null, Some = quelque chose)
+  }) = _SignInFormState;
+
+  factory SignInFormState.initial() => SignInFormState(
+    emailAddress: EmailAddress(''),
+    password: Password(''),
+    showErrorMessages: false,
+    isSubmiting: false,
+    authFailureOrSuccessOption: none(),
+
+  );
+}
